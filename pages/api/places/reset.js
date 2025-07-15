524e9bce4a74892d91f7a5c288bc8290f67aa6af
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-15 11:29:49 +0200

    initiliazed server connection and filled Mongo with places

--- pages/api/places/reset.js
+++ pages/api/places/reset.js
@@ -1,9 +1,11 @@
 import dbConnect from "@/db/connect";
+import { places } from "../../../lib/db";
 import Place from "@/db/Schema/Place";
 
 export default async function handler(_, response) {
   await dbConnect();
-  const places= await Place.find();
+  await Place.deleteMany();
+  await Place.create(places);
 
   response.status(200).json(places);
   return;
