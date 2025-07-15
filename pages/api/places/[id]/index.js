ee2853131d22fe96c55c9beb6b9f8069531e3df0
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-15 15:23:16 +0200

    updating

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -4,12 +4,25 @@ import Place from "@/db/Schema/Place";
 export default async function handler(request, response) {
   const { id } = request.query;
   await dbConnect();
-  const place = await Place.findById(id);
-
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    if (!place) {
+      response.status(404).json({ status: "Not found" });
+      return;
+    }
+    response.status(200).json(place);
     return;
   }
 
-  response.status(200).json(place);
+  if (request.method === "PUT") {
+    try {
+      const place = request.body;
+      await Place.findByIdAndUpdate(id, place);
+      response.status(201).json({ status: "updated" });
+      return;
+    } catch (error) {
+      response.status(500).json({ error: error });
+      return;
+    }
+  }
 }
