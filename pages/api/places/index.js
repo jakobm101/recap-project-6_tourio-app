8746dec3139b73ad565ac1430866a7092affc7d3
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-15 13:11:25 +0200

    added GET and 500

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,10 +1,18 @@
 import dbConnect from "@/db/connect";
 import Place from "@/db/Schema/Place";
 
-export default async function handler(_, response) {
+export default async function handler(request, response) {
   await dbConnect();
-  const places= await Place.find();
-
-  response.status(200).json(places);
-  return;
+  if (request.method === "GET") {
+    try {
+      const places = await Place.find();
+      response.status(200).json(places);
+      return;
+    } catch (error) {
+      response.status(500).json({ error: error.message });
+      return;
+    }
+  } else {
+    response.status(500).json({ message: "Not allowed" });
+  }
 }
